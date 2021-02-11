d70f75a67ff69ec72b032b92771f40c82f5edcba
svsosm <savasosmnglu@gmail.com>
2021-02-12 02:42:49 +0300

    Try Selection Cell Neighbour
    HexMap script edited.
    Create HexNeighbours script.
    Try to select cell neighbours.

--- HexagonSavas/Assets/Script/HexMap.cs
+++ HexagonSavas/Assets/Script/HexMap.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class HexMap : MonoBehaviour
 {
+    public static HexMap instance;
+
     public GameObject hexPrefab;
     public Color[] colors;
+    public GameObject[] hexCells;
+    public Vector3[] cellPositions;
 
     public int width = 8;
     public int height = 9;
@@ -12,30 +17,56 @@ public class HexMap : MonoBehaviour
     float xOffset = 0.538f;
     float yOffset = 0.469f;
 
+    private void Awake()
+    {
+        instance = this;
+    }
+
     private void Start()
     {
-        for(int x=0; x<width; x++)
-        {
-            for(int y=0; y<height; y++)
-            {
-                float xPos = x * xOffset;
-                if(y % 2 == 1)
-                {
-                    xPos += xOffset / 2f;
-                }
-                hexPrefab.GetComponent<SpriteRenderer>().color = chooseRandomHexColor();
-                GameObject hex_go = (GameObject) Instantiate(hexPrefab, new Vector3(-2 + xPos, -2f + y * yOffset, 0), Quaternion.identity);
-                hex_go.name = "Hex " + x + "_" + y;
-                hex_go.transform.SetParent(this.transform);
-                
 
+        GenerateHexMap();
+
+    }
+
+    //generate hex map
+    void GenerateHexMap()
+    {
+        hexCells = new GameObject[width * height]; //store cell object
+        cellPositions = new Vector3[width * height]; //store cell position
+        for(int x=0, i=0; x < width; x++)
+        {
+            for (int y=0; y < height; y++)
+            {
+                CreateHexCell(x, y, i++);
             }
         }
     }
 
+    //create cell in hexmap
+    void CreateHexCell(int x, int y, int i)
+    {
+        Vector3 hexPos;
+        float xPos = x * xOffset;
+        if (y % 2 == 1)
+        {
+            xPos += xOffset / 2f;
+        }
+        hexPos.x = -2f + xPos;
+        hexPos.y = -2f + y * yOffset;
+        hexPos.z = 0f;
+        hexPrefab.GetComponent<SpriteRenderer>().color = chooseRandomHexColor();
+        GameObject hex_go = hexCells[i] = Instantiate(hexPrefab, new Vector3(hexPos.x, hexPos.y, hexPos.z), Quaternion.identity) as GameObject;
+        cellPositions[i] = hexPos;
+        hex_go.name = x + "_" + y;
+        hex_go.transform.SetParent(this.transform);
+    }
+
+
+    //create different color cell
     Color chooseRandomHexColor()
     {
-        int randNum = Random.Range(0, 5);
+        int randNum = Random.Range(0, 5); //create random number for select different color
         Color32 resultColor = new Color32();
         switch(randNum)
         {
---
+++ HexagonSavas/Assets/Script/HexNeighbour.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexNeighbour : MonoBehaviour
+{
+
+    private void Update()
+    {
+        Ray worldPos = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(worldPos, out hit))
+        {
+            Debug.Log(hit.point);
+        }
+    }
+
+    public GameObject[] GetNeighbours(int x, int y, int mapSizeX, int mapSizeY, Vector3 cellPosition)
+    {
+        List<GameObject> neighbours = new List<GameObject>(2);
+
+        
+        if(x > cellPosition.x && y > cellPosition.y)
+        {
+            //TOP RIGHT + RIGHT
+            neighbours.Add(HexMap.instance.hexCells[x + 1 + y * mapSizeY]);
+            neighbours.Add(HexMap.instance.hexCells[x + (y - 1) * mapSizeY]);
+        }
+        else if(x < cellPosition.x && y > cellPosition.y)
+        {
+            //TOP LEFT + LEFT
+            neighbours.Add(HexMap.instance.hexCells[x - 1 + y * mapSizeY]);
+            neighbours.Add(HexMap.instance.hexCells[(x - 1) + (y - 1) * mapSizeY]);
+        }
+
+
+        //LEFT
+        if (x > 0)
+            neighbours.Add(HexMap.instance.hexCells[x - 1 + y * mapSizeY]);
+
+        //RIGHT
+        if (x < mapSizeX)
+            neighbours.Add(HexMap.instance.hexCells[x + 1 + y * mapSizeY]);
+        
+        if(y % 2 == 0)
+        {
+            //TOP LEFT
+            if (y > 0)
+                neighbours.Add(HexMap.instance.hexCells[(x - 1) + (y - 1) * mapSizeY]);
+            
+            
+        }
+        return neighbours.ToArray();
+    }
+}

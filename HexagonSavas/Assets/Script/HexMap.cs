cb168fb2276e560ae1d96b1975e4fa5154e5bc0e
svsosm <savasosmnglu@gmail.com>
2021-02-13 22:27:08 +0300

    Select hex group
    add selected cell image.
    fixed random cells color.
    add select hex groups.

--- HexagonSavas/Assets/Script/HexMap.cs
+++ HexagonSavas/Assets/Script/HexMap.cs
@@ -6,16 +6,19 @@ public class HexMap : MonoBehaviour
 {
     public static HexMap instance;
 
+    
+
     public GameObject hexPrefab;
     public Color[] colors;
     public GameObject[] hexCells;
-    public Vector3[] cellPositions;
 
     public int width = 8;
     public int height = 9;
 
     float xOffset = 0.538f;
     float yOffset = 0.469f;
+
+    int randNum;
 
     private void Awake()
     {
@@ -29,11 +32,12 @@ public class HexMap : MonoBehaviour
 
     }
 
+
+    #region HEXAGONGRID
     //generate hex map
     void GenerateHexMap()
     {
         hexCells = new GameObject[width * height]; //store cell object
-        cellPositions = new Vector3[width * height]; //store cell position
         for(int x=0, i=0; x < width; x++)
         {
             for (int y=0; y < height; y++)
@@ -52,44 +56,114 @@ public class HexMap : MonoBehaviour
         {
             xPos += xOffset / 2f;
         }
-        hexPos.x = -2f + xPos;
-        hexPos.y = -2f + y * yOffset;
+        hexPos.x = xPos;
+        hexPos.y = y * yOffset;
         hexPos.z = 0f;
         hexPrefab.GetComponent<SpriteRenderer>().color = chooseRandomHexColor();
         GameObject hex_go = hexCells[i] = Instantiate(hexPrefab, new Vector3(hexPos.x, hexPos.y, hexPos.z), Quaternion.identity) as GameObject;
-        cellPositions[i] = hexPos;
-        hex_go.name = x + "_" + y;
+        hex_go.name = i.ToString();
         hex_go.transform.SetParent(this.transform);
+        ControlNeighbourColor(hex_go, i, x, y);
+        
+    }
+    #endregion
+
+    #region HEXCOLOR
+    void ControlNeighbourColor(GameObject mainCell, int i, int X, int Y)
+    {
+        List<Color> selectedCells = new List<Color>();
+        selectedCells.Add(AccessGameObjectColor(mainCell));
+        if (X == 1 && Y == 0)
+        {
+            selectedCells.Add(AccessGameObjectColor(hexCells[i - height + 1]));
+            selectedCells.Add(AccessGameObjectColor(hexCells[i - height]));
+
+            checkNeighbourColor(selectedCells, mainCell);
+            
+        }
+        else if(X > 0 && Y == 1)
+        {
+            //check second column
+            selectedCells.Add(AccessGameObjectColor(hexCells[i - height]));
+            selectedCells.Add(AccessGameObjectColor(hexCells[i - 1]));
+
+            checkNeighbourColor(selectedCells, mainCell);
+
+        }
+        else if(X == width - 1 && Y == height - 1)
+        {
+            selectedCells.Add(AccessGameObjectColor(hexCells[i - height]));
+            selectedCells.Add(AccessGameObjectColor(hexCells[i - height - 1]));
+            selectedCells.Add(AccessGameObjectColor(hexCells[i - 1]));
+
+            checkNeighbourColor(selectedCells, mainCell);
+        }
+        else if(X == width-1 && Y % 2 != 0 && Y > 2)
+        {
+            //check last column with odd index
+            selectedCells.Add(AccessGameObjectColor(hexCells[i - height]));
+            selectedCells.Add(AccessGameObjectColor(hexCells[i - 1]));
+
+            checkNeighbourColor(selectedCells, mainCell);
+        }
+        else if(X > 0 && Y > 0) 
+        {
+            //check other cells
+            selectedCells.Add(AccessGameObjectColor(hexCells[i - height + 1]));
+            selectedCells.Add(AccessGameObjectColor(hexCells[i - height]));
+            selectedCells.Add(AccessGameObjectColor(hexCells[i - height - 1]));
+            selectedCells.Add(AccessGameObjectColor(hexCells[i - 1]));
+
+            checkNeighbourColor(selectedCells, mainCell);
+        }
+
+
+    }
+
+    //if main object and neighbours are same color, change main cell color
+    void checkNeighbourColor(List<Color> colors, GameObject mainCell)
+    {
+        for (int index = 1; index < colors.Count - 1; index++)
+        {
+            if (colors[0] == colors[index] && colors[0] == colors[index + 1])
+            {
+                mainCell.GetComponent<SpriteRenderer>().color = colors[0] = changeHexColor(colors[0]);
+            }
+        }
+    }
+
+
+    //acces gameobject sprite color
+    Color AccessGameObjectColor(GameObject gameObject)
+    {
+        Color goColor = gameObject.GetComponent<SpriteRenderer>().color;
+        return goColor;
+    }
+
+
+    //if new color is same existcolor, change it.
+    Color changeHexColor(Color existColor)
+    {
+        Color newColor = chooseRandomHexColor();
+        if (existColor == newColor)
+        {
+            changeHexColor(newColor);
+        }
+
+        return newColor;
     }
 
 
     //create different color cell
     Color chooseRandomHexColor()
     {
-        int randNum = Random.Range(0, 5); //create random number for select different color
-        Color32 resultColor = new Color32();
-        switch(randNum)
-        {
-            case 0:
-                resultColor = colors[0];
-                break;
-            case 1:
-                resultColor = colors[1];
-                break;
-            case 2:
-                resultColor = colors[2];
-                break;
-            case 3:
-                resultColor = colors[3];
-                break;
-            case 4:
-                resultColor = colors[4];
-                break;
-        }
+        randNum = Random.Range(0, 5); //create random number for select different color
+        Color resultColor = new Color();
+        resultColor = colors[randNum];
         return resultColor;
     }
 
-
+    #endregion HEXCOLOR
 
 
 }
--- HexagonSavas/Assets/Script/HexNeighbour.cs
+++ HexagonSavas/Assets/Script/HexNeighbour.cs
@@ -5,51 +5,257 @@ using UnityEngine;
 public class HexNeighbour : MonoBehaviour
 {
 
+    public Sprite selectedSprite;
+    List<GameObject> selectedHexGroup;
+
+    int width;
+    int height;
+    public int clickCount;
+    string oldClickCellName;
+
+    private void Start()
+    {
+        height = HexMap.instance.height;
+        width = HexMap.instance.width;
+    }
     private void Update()
     {
-        Ray worldPos = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if(Physics.Raycast(worldPos, out hit))
+        if(Input.GetMouseButtonDown(0))
         {
-            Debug.Log(hit.point);
+
+            SelectHexCell();
+
         }
     }
 
-    public GameObject[] GetNeighbours(int x, int y, int mapSizeX, int mapSizeY, Vector3 cellPosition)
+    void SelectHexCell()
     {
-        List<GameObject> neighbours = new List<GameObject>(2);
-
-        
-        if(x > cellPosition.x && y > cellPosition.y)
+        Debug.Log("Mouse is down");
+        RaycastHit2D hitInfo = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+        if (hitInfo.collider != null)
         {
-            //TOP RIGHT + RIGHT
-            neighbours.Add(HexMap.instance.hexCells[x + 1 + y * mapSizeY]);
-            neighbours.Add(HexMap.instance.hexCells[x + (y - 1) * mapSizeY]);
+            if (hitInfo.transform.gameObject.tag == "HexCell")
+            {
+                
+                if (oldClickCellName == hitInfo.transform.gameObject.name)
+                {
+                    clickCount++;
+                }
+                else
+                {
+                    clickCount = 1;
+                }
+                oldClickCellName = hitInfo.transform.gameObject.name;
+                SelectAllNeigbourAroundSelectedCell(hitInfo.transform.gameObject, int.Parse(oldClickCellName));
+                
+            }
+            else
+            {
+                Debug.Log("nop");
+            }
         }
-        else if(x < cellPosition.x && y > cellPosition.y)
+        else
         {
-            //TOP LEFT + LEFT
-            neighbours.Add(HexMap.instance.hexCells[x - 1 + y * mapSizeY]);
-            neighbours.Add(HexMap.instance.hexCells[(x - 1) + (y - 1) * mapSizeY]);
+            Debug.Log("no hit");
         }
-
-
-        //LEFT
-        if (x > 0)
-            neighbours.Add(HexMap.instance.hexCells[x - 1 + y * mapSizeY]);
-
-        //RIGHT
-        if (x < mapSizeX)
-            neighbours.Add(HexMap.instance.hexCells[x + 1 + y * mapSizeY]);
-        
-        if(y % 2 == 0)
-        {
-            //TOP LEFT
-            if (y > 0)
-                neighbours.Add(HexMap.instance.hexCells[(x - 1) + (y - 1) * mapSizeY]);
-            
-            
-        }
-        return neighbours.ToArray();
     }
+
+    #region SELECT NEIGHBOURS
+    void SelectAllNeigbourAroundSelectedCell(GameObject selectHex, int i)
+    {
+        selectedHexGroup = new List<GameObject>();
+        selectedHexGroup.Add(HexMap.instance.hexCells[i]);
+        if(i == 0) //select bottom left cell
+        {
+            selectedHexGroup.Add(HexMap.instance.hexCells[i + 1]);
+            selectedHexGroup.Add(HexMap.instance.hexCells[i + width + 1]);
+        }
+        else if(i == height - 1) //select top left cell
+        {
+            selectedHexGroup.Add(HexMap.instance.hexCells[i - 1]);
+            selectedHexGroup.Add(HexMap.instance.hexCells[i + width + 1]);
+        }
+        else if(i == Mathf.Pow(width,2) - 1) //select bottom right cell
+        {
+            selectedHexGroup.Add(HexMap.instance.hexCells[i + 1]);
+            selectedHexGroup.Add(HexMap.instance.hexCells[i - width]);
+            selectedHexGroup.Add(HexMap.instance.hexCells[i - width - 1]);
+        }
+        else if(i == (height * width) - 1) //select top right cell
+        {
+            selectedHexGroup.Add(HexMap.instance.hexCells[i - height]);
+            selectedHexGroup.Add(HexMap.instance.hexCells[i - height - 1]);
+            selectedHexGroup.Add(HexMap.instance.hexCells[i - 1]);
+        }
+        else if(i % 2 == 0 && i < height - 1 && i > 0) // select cell with even index in the left corner
+        {
+            selectedHexGroup.Add(HexMap.instance.hexCells[i + 1]);
+            selectedHexGroup.Add(HexMap.instance.hexCells[i + width +1]);
+            selectedHexGroup.Add(HexMap.instance.hexCells[i - 1]);
+        }
+        else if(i % 2 == 0 && i > Mathf.Pow(width,2) - 1 && i < height * width - 1) // select cell with even index in the right corner
+        {
+            selectedHexGroup.Add(HexMap.instance.hexCells[i + 1]);
+            selectedHexGroup.Add(HexMap.instance.hexCells[i - width - 1]);
+            selectedHexGroup.Add(HexMap.instance.hexCells[i - 1]);
+        }
+        else if (i % 2 != 0 && i < height - 1 && i > 0) // select cell with odd index in the left corner
+        {
+            selectedHexGroup.Add(HexMap.instance.hexCells[i + 1]);
+            selectedHexGroup.Add(HexMap.instance.hexCells[i + width + 2]);
+            selectedHexGroup.Add(HexMap.instance.hexCells[i +width + 1]);
+            selectedHexGroup.Add(HexMap.instance.hexCells[i + width]);
+            selectedHexGroup.Add(HexMap.instance.hexCells[i - 1]);
+        }
+        else if (i % 2 != 0 && i > Mathf.Pow(width,2) - 1 && i < height * width - 1) // select cell with odd index in the right corner
+        {
+            selectedHexGroup.Add(HexMap.instance.hexCells[i + 1]);
+            selectedHexGroup.Add(HexMap.instance.hexCells[i -width]);
+            selectedHexGroup.Add(HexMap.instance.hexCells[i - width -1]);
+            selectedHexGroup.Add(HexMap.instance.hexCells[i - width - 2]);
+            selectedHexGroup.Add(HexMap.instance.hexCells[i - 1]);
+
+        }
+        else if(i % height == 0 && i > 0 && i < Mathf.Pow(width,2)-1) // select bottom row cell
+        {
+            selectedHexGroup.Add(HexMap.instance.hexCells[i - height]);
+            selectedHexGroup.Add(HexMap.instance.hexCells[i - height + 1]);
+            selectedHexGroup.Add(HexMap.instance.hexCells[i + 1]);
+            selectedHexGroup.Add(HexMap.instance.hexCells[i + height]);
+        }
+        else if(i % height == width && i > width && i < height*width -1) // select top row cell
+        {
+            selectedHexGroup.Add(HexMap.instance.hexCells[i - height]);
+            selectedHexGroup.Add(HexMap.instance.hexCells[i - height - 1]);
+            selectedHexGroup.Add(HexMap.instance.hexCells[i - 1]);
+            selectedHexGroup.Add(HexMap.instance.hexCells[i + height]);
+        }
+        else //select center cell
+        {
+            selectedHexGroup.Add(HexMap.instance.hexCells[i + 1]);
+            selectedHexGroup.Add(HexMap.instance.hexCells[i - height]);
+            selectedHexGroup.Add(HexMap.instance.hexCells[i - 1]);
+            selectedHexGroup.Add(HexMap.instance.hexCells[i + height - 1]);
+            selectedHexGroup.Add(HexMap.instance.hexCells[i + height]);
+            selectedHexGroup.Add(HexMap.instance.hexCells[i + height + 1]);
+        }
+        SelectHexCells(selectedHexGroup);
+
+    }
+
+
+
+    /**********************************************
+     * if cell have 2 neighbours, it has 1 selected hex groups situation  (mainCell + 2 neighbours)
+     * have 3 neighbours, 2 situation
+     * have 4 neighbours, 3 situation
+     * have 5 neighbours, 4 situation
+     * have 6 neighbours, 5 situation
+     * 
+     * Each click select different hex group.
+     * normally,neighbours in the clicked corner of the selected cell should be selected.but no time for this.
+     * 
+     **********************************************/
+    void SelectHexCells(List<GameObject> selectedHexGroup)
+    {
+        List<GameObject> selectedHexCells = new List<GameObject>();
+
+        switch(selectedHexGroup.Count)
+        {
+            case 3:
+                selectedHexCells = selectedHexGroup;
+                break;
+            case 4:
+                if(clickCount % (selectedHexGroup.Count-2) == 1) //first click
+                {
+                    SeperateAllSituationForHexGroups(1, selectedHexCells);
+                }
+                else if(clickCount % (selectedHexGroup.Count - 2) == 0 && clickCount > 0) //second click
+                {
+                    SeperateAllSituationForHexGroups(2, selectedHexCells);
+                }
+                break;
+            case 5:
+                if (clickCount % (selectedHexGroup.Count - 2) == 1) //first click
+                {
+                    SeperateAllSituationForHexGroups(1, selectedHexCells);
+                }
+                else if(clickCount % (selectedHexGroup.Count - 2) == 2) //second click
+                {
+                    SeperateAllSituationForHexGroups(2, selectedHexCells); 
+                }
+                else if (clickCount % (selectedHexGroup.Count - 2) == 0 && clickCount > 0) //third click
+                {
+                    SeperateAllSituationForHexGroups(3, selectedHexCells);
+                }
+                break;
+            case 6:
+                if (clickCount % (selectedHexGroup.Count - 2) == 1) //first click
+                {
+                    SeperateAllSituationForHexGroups(1, selectedHexCells);
+                }
+                else if (clickCount % (selectedHexGroup.Count - 2) == 2) //second click
+                {
+                    SeperateAllSituationForHexGroups(2, selectedHexCells);
+                }
+                else if(clickCount % (selectedHexGroup.Count - 2) == 3) //third click
+                {
+                    SeperateAllSituationForHexGroups(3, selectedHexCells);
+
+                }
+                else if (clickCount % (selectedHexGroup.Count - 2) == 0 && clickCount > 0) //fourth click
+                {
+                    SeperateAllSituationForHexGroups(4, selectedHexCells);
+                }
+                break;
+            case 7:
+                if (clickCount % (selectedHexGroup.Count - 2) == 1) //first click
+                {
+                    SeperateAllSituationForHexGroups(1, selectedHexCells);
+                }
+                else if (clickCount % (selectedHexGroup.Count - 2) == 2)//second click
+                {
+                    SeperateAllSituationForHexGroups(2, selectedHexCells);
+                }
+                else if (clickCount % (selectedHexGroup.Count - 2) == 3) //third click
+                {
+                    SeperateAllSituationForHexGroups(3, selectedHexCells);
+
+                }
+                else if (clickCount % (selectedHexGroup.Count - 2) == 4)//fourth click
+                {
+                    SeperateAllSituationForHexGroups(4, selectedHexCells);
+
+                }
+                else if (clickCount % (selectedHexGroup.Count - 2) == 0 && clickCount > 0) //fifth click
+                {
+                    SeperateAllSituationForHexGroups(5, selectedHexCells);
+                }
+                break;
+        }
+
+        foreach (GameObject a in selectedHexCells)
+        {
+            Debug.Log(a.name);
+        }
+
+
+
+    }
+
+    void SeperateAllSituationForHexGroups(int count, List<GameObject> hexCells)
+    {
+        for (int i = count; i < count + 1; i++)
+        {
+            hexCells.Add(selectedHexGroup[0]);
+            hexCells.Add(selectedHexGroup[i]);
+            hexCells.Add(selectedHexGroup[i + 1]);
+        }
+
+    }
+
+    #endregion
+
+
+
 }
